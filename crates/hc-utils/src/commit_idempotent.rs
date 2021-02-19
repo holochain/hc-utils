70f4c8ac961d338d67d4221b7478b8a4f3cb61ac
Guillem Cordoba <guillem.cordoba@gmail.com>
2021-02-19 17:50:20 +0100

    Fixed bug

--- crates/hc-utils/src/commit_idempotent.rs
+++ crates/hc-utils/src/commit_idempotent.rs
@@ -5,7 +5,7 @@ use hdk::prelude::*;
 /// Query for an existing Entry in the local source-chain matching the given EntryType name(s).  If
 /// one exists, return it Address, otherwise commit it.
 pub fn commit_idempotent(entry_id: String, value: Entry) -> UtilsResult<HeaderHash> {
-    for element in local_source_chain().unwrap().0 {
+    for element in local_source_chain().unwrap() {
         if let element::ElementEntry::Present(entry) = element.entry() {
             if entry.clone() == value {
                 return Ok(element.header_address().clone());
--- crates/hc-utils/src/exists.rs
+++ crates/hc-utils/src/exists.rs
@@ -5,7 +5,7 @@ use hdk::prelude::*;
 /// Query for an existing Entry in the local source-chain matching the given EntryType name(s).  If
 /// one exists, return it Address, otherwise returns error.
 pub fn exists(value: Entry) -> UtilsResult<HeaderHash> {
-    for element in local_source_chain().unwrap().0 {
+    for element in local_source_chain().unwrap() {
         if let element::ElementEntry::Present(entry) = element.entry() {
             if entry.clone() == value {
                 return Ok(element.header_address().clone());
