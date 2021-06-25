2ce3ae7d1f26ed038663a608c5dad3de29b1875e
zo-el <joelulahanna@gmail.com>
2021-06-25 14:49:15 +0530

    update to get url safe hashes

--- crates/hc-utils/src/wrappers.rs
+++ crates/hc-utils/src/wrappers.rs
@@ -42,7 +42,7 @@ impl From<WrappedAgentPubKey> for AgentPubKey {
 
 impl From<WrappedAgentPubKey> for HashString {
     fn from(wrapped_agent_pub_key: WrappedAgentPubKey) -> Self {
-        Self(wrapped_agent_pub_key.0.to_string())
+        Self(format!("{:?}", wrapped_agent_pub_key.0))
     }
 }
 
@@ -57,7 +57,7 @@ impl TryFrom<HashString> for WrappedHeaderHash {
 }
 impl From<WrappedHeaderHash> for HashString {
     fn from(wrapped_header_hash: WrappedHeaderHash) -> Self {
-        Self(wrapped_header_hash.0.to_string())
+        Self(format!("{:?}", wrapped_header_hash.0))
     }
 }
 
@@ -72,7 +72,7 @@ impl TryFrom<HashString> for WrappedEntryHash {
 }
 impl From<WrappedEntryHash> for HashString {
     fn from(wrapped_entry_hash: WrappedEntryHash) -> Self {
-        Self(wrapped_entry_hash.0.to_string())
+        Self(format!("{:?}", wrapped_entry_hash.0))
     }
 }
 
@@ -86,7 +86,7 @@ impl TryFrom<HashString> for WrappedDnaHash {
     }
 }
 impl From<WrappedDnaHash> for HashString {
-    fn from(wrapped_entry_hash: WrappedDnaHash) -> Self {
-        Self(wrapped_entry_hash.0.to_string())
+    fn from(wrapped_dna_hash: WrappedDnaHash) -> Self {
+        Self(format!("{:?}", wrapped_dna_hash.0))
     }
 }
