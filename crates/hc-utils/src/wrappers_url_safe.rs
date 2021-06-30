f6b44cdf6feaaccd5012a094f9a76b4584e9b202
zo-el <joelulahanna@gmail.com>
2021-06-30 16:27:33 +0530

    add substring

--- crates/hc-utils/src/wrappers_url_safe.rs
+++ crates/hc-utils/src/wrappers_url_safe.rs
@@ -27,8 +27,10 @@ pub struct SafeWrappedDnaHash(pub DnaHash);
 impl TryFrom<HashString> for SafeWrappedAgentPubKey {
     type Error = String;
     fn try_from(ui_string_hash: HashString) -> Result<Self, Self::Error> {
-        assert_eq!(59, ui_string_hash.0.len());
-        match multibase::decode(ui_string_hash.0) {
+        let mut hash = ui_string_hash.0;
+        hash.replace_range(..3, "");
+        assert_eq!(59, hash.len());
+        match multibase::decode(hash) {
             Ok((_, bytes)) => Ok(Self(holo_hash::AgentPubKey::from_raw_36_and_type(bytes, holo_hash::hash_type::Agent))),
             Err(e) => Err(format!("{:?}", e)),
         }
@@ -43,15 +45,18 @@ impl From<SafeWrappedAgentPubKey> for AgentPubKey {
 
 impl From<SafeWrappedAgentPubKey> for HashString {
     fn from(wrapped_agent_pub_key: SafeWrappedAgentPubKey) -> Self {
-        Self(multibase::encode(multibase::Base::Base32Lower, &wrapped_agent_pub_key.0.get_raw_39()[3..]).to_string())
+        let hash = multibase::encode(multibase::Base::Base32Lower, &wrapped_agent_pub_key.0.get_raw_39()[3..]);
+        Self(format!("hcak{}", hash.to_string()))
     }
 }
 
 impl TryFrom<HashString> for SafeWrappedHeaderHash {
     type Error = String;
     fn try_from(ui_string_hash: HashString) -> Result<Self, Self::Error> {
-        assert_eq!(59, ui_string_hash.0.len());
-        match multibase::decode(ui_string_hash.0) {
+        let mut hash = ui_string_hash.0;
+        hash.replace_range(..3, "");
+        assert_eq!(59, hash.len());
+        match multibase::decode(hash) {
             Ok((_, bytes)) => Ok(Self(holo_hash::HeaderHash::from_raw_36_and_type(bytes, holo_hash::hash_type::Header))),
             Err(e) => Err(format!("{:?}", e)),
         }
@@ -59,15 +64,18 @@ impl TryFrom<HashString> for SafeWrappedHeaderHash {
 }
 impl From<SafeWrappedHeaderHash> for HashString {
     fn from(wrapped_header_hash: SafeWrappedHeaderHash) -> Self {
-        Self(multibase::encode(multibase::Base::Base32Lower, &wrapped_header_hash.0.get_raw_39()[3..]).to_string())
+        let hash = multibase::encode(multibase::Base::Base32Lower, &wrapped_header_hash.0.get_raw_39()[3..]);
+        Self(format!("hckk{}", hash.to_string()))
     }
 }
 
 impl TryFrom<HashString> for SafeWrappedEntryHash {
     type Error = String;
     fn try_from(ui_string_hash: HashString) -> Result<Self, Self::Error> {
-        assert_eq!(59, ui_string_hash.0.len());
-        match multibase::decode(ui_string_hash.0) {
+        let mut hash = ui_string_hash.0;
+        hash.replace_range(..3, "");
+        assert_eq!(59, hash.len());
+        match multibase::decode(hash) {
             Ok((_, bytes)) => Ok(Self(holo_hash::EntryHash::from_raw_36_and_type(bytes, holo_hash::hash_type::Entry))),
             Err(e) => Err(format!("{:?}", e)),
         }
@@ -75,15 +83,18 @@ impl TryFrom<HashString> for SafeWrappedEntryHash {
 }
 impl From<SafeWrappedEntryHash> for HashString {
     fn from(wrapped_entry_hash: SafeWrappedEntryHash) -> Self {
-        Self(multibase::encode(multibase::Base::Base32Lower, &wrapped_entry_hash.0.get_raw_39()[3..]).to_string())
+        let hash = multibase::encode(multibase::Base::Base32Lower, &wrapped_entry_hash.0.get_raw_39()[3..]);
+        Self(format!("hcek{}", hash.to_string()))
     }
 }
 
 impl TryFrom<HashString> for SafeWrappedDnaHash {
     type Error = String;
     fn try_from(ui_string_hash: HashString) -> Result<Self, Self::Error> {
-        assert_eq!(59, ui_string_hash.0.len());
-        match multibase::decode(ui_string_hash.0) {
+        let mut hash = ui_string_hash.0;
+        hash.replace_range(..3, "");
+        assert_eq!(59, hash.len());
+        match multibase::decode(hash) {
             Ok((_, bytes)) => Ok(Self(holo_hash::DnaHash::from_raw_36_and_type(bytes, holo_hash::hash_type::Dna))),
             Err(e) => Err(format!("{:?}", e)),
         }
@@ -91,6 +102,7 @@ impl TryFrom<HashString> for SafeWrappedDnaHash {
 }
 impl From<SafeWrappedDnaHash> for HashString {
     fn from(wrapped_dna_hash: SafeWrappedDnaHash) -> Self {
-        Self(multibase::encode(multibase::Base::Base32Lower, &wrapped_dna_hash.0.get_raw_39()[3..]).to_string())
+        let hash = multibase::encode(multibase::Base::Base32Lower, &wrapped_dna_hash.0.get_raw_39()[3..]);
+        Self(format!("hc0k{}", hash.to_string()))
     }
 }
