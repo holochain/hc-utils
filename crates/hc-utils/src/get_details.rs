f33f4c460573858a3242465675a7de47eefcedb3
zo-el <joelulahanna@gmail.com>
2022-10-26 20:24:38 -0500

    version bump 0.0.157

--- crates/hc-utils/src/get_details.rs
+++ crates/hc-utils/src/get_details.rs
@@ -4,9 +4,20 @@ use link::Link;
 
 /// optimized get details by links
 pub fn get_details(links: Vec<Link>, option: GetOptions) -> UtilsResult<Vec<Option<Details>>> {
+    let links: Vec<AnyDhtHash> = links
+        .iter()
+        .filter_map(|l| match l.clone().target.into_entry_hash() {
+            Some(e) => Some(e.into()),
+            None => match l.clone().target.into_action_hash() {
+                Some(a) => Some(a.into()),
+                None => None,
+            },
+        })
+        .collect();
+
     let msg_results_input: Vec<GetInput> = links
         .into_iter()
-        .map(|link| GetInput::new(link.target.into(), option.clone()))
+        .map(|link| GetInput::new(link, option.clone()))
         .collect();
     Ok(HDK.with(|hdk| hdk.borrow().get_details(msg_results_input))?)
 }
