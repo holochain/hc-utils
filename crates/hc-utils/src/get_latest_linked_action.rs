881da567df7f348e0add0ad408a96742bd4b02a1
zo-el <joelulahanna@gmail.com>
2023-05-03 14:06:19 -0500

    version bump 0.2.0

--- crates/hc-utils/src/get_latest_linked_action.rs
+++ crates/hc-utils/src/get_latest_linked_action.rs
@@ -6,10 +6,11 @@ pub fn get_latest_linked_action(
     link_tag: Option<LinkTag>,
 ) -> ExternResult<Vec<Record>> {
     let links = get_links(base, link_type, link_tag)?;
+
     let records: Vec<Record> = links
         .into_iter()
         .filter_map(
-            |link| match helper_get_latest_action(ActionHash::from(link.target).into()) {
+            |link| match helper_get_latest_action(link.target.into_any_dht_hash()) {
                 Ok(a) => a,
                 _ => None,
             },
@@ -19,21 +20,28 @@ pub fn get_latest_linked_action(
     Ok(records)
 }
 
-fn helper_get_latest_action(action_hash: ActionHash) -> ExternResult<Option<Record>> {
-    let details = get_details(action_hash, GetOptions::default())?.ok_or(wasm_error!(
-        WasmErrorInner::Guest("Action not found".into())
-    ))?;
-    let record_details = match details {
-        Details::Entry(_) => Err(wasm_error!(WasmErrorInner::Guest(
-            "Malformed details".into()
-        ))),
-        Details::Record(record_details) => Ok(record_details),
-    }?;
-    if record_details.deletes.len() > 0 {
-        return Ok(None);
-    }
-    match record_details.updates.last() {
-        Some(update) => helper_get_latest_action(update.action_address().clone()),
-        None => Ok(Some(record_details.record)),
+fn helper_get_latest_action<H: Into<AnyDhtHash>>(
+    action_hash: Option<H>,
+) -> ExternResult<Option<Record>> {
+    match action_hash {
+        Some(hash) => {
+            let details = get_details(hash, GetOptions::default())?.ok_or(wasm_error!(
+                WasmErrorInner::Guest("Action not found".into())
+            ))?;
+            let record_details = match details {
+                Details::Entry(_) => Err(wasm_error!(WasmErrorInner::Guest(
+                    "Malformed details".into()
+                ))),
+                Details::Record(record_details) => Ok(record_details),
+            }?;
+            if record_details.deletes.len() > 0 {
+                return Ok(None);
+            }
+            match record_details.updates.last() {
+                Some(update) => helper_get_latest_action(Some(update.action_address().clone())),
+                None => Ok(Some(record_details.record)),
+            }
+        }
+        None => Ok(None),
     }
 }
